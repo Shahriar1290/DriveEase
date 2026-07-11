6a540c7211ad7a4bfe45dfcc4177e4f36c5a8f02
Shahriar Hossain Prottoy <shahriarh346@gmail.com>
2026-07-11 09:45:00 +0600

    Update seeders with model-specific vehicle images

--- database/seeders/VehicleSeeder.php
+++ database/seeders/VehicleSeeder.php
@@ -73,7 +73,81 @@ class VehicleSeeder extends Seeder
             ['Toyota Land Cruiser','Toyota','Land Cruiser','extra',2,'diesel','automatic',8,200.00,'available',2022,'White'],
         ];
 
-        $imageUrls = ['vehicles/car1.jpg', 'vehicles/car2.jpg', 'vehicles/car3.jpg', 'vehicles/car4.jpg', 'vehicles/car5.jpg'];
+        $vehicleImages = [
+            // Sedans
+            'Toyota Camry'          => 'vehicles/camry.jpg',
+            'Honda Accord'          => 'vehicles/accord.jpg',
+            'Nissan Altima'         => 'vehicles/altima.jpg',
+            'Ford Fusion'           => 'vehicles/fusion.jpg',
+            'Hyundai Sonata'        => 'vehicles/sonata.jpg',
+            'Mazda 6'               => 'vehicles/mazda6.jpg',
+            'Chevrolet Malibu'      => 'vehicles/malibu.jpg',
+            // SUVs
+            'Toyota RAV4'           => 'vehicles/rav4.jpg',
+            'Honda CR-V'            => 'vehicles/crv.jpg',
+            'Ford Explorer'         => 'vehicles/explorer.jpg',
+            'Chevrolet Equinox'     => 'vehicles/equinox.jpg',
+            'Hyundai Tucson'        => 'vehicles/tucson.jpg',
+            'Nissan Rogue'          => 'vehicles/rogue.jpg',
+            'Mazda CX-5'            => 'vehicles/suv1.jpg',
+            'Kia Sportage'          => 'vehicles/suv2.jpg',
+            // Hatchbacks
+            'Toyota Yaris'          => 'vehicles/yaris.jpg',
+            'Honda Jazz'            => 'vehicles/jazz.jpg',
+            'Hyundai i20'           => 'vehicles/i20.jpg',
+            'Ford Fiesta'           => 'vehicles/fiesta.jpg',
+            'Volkswagen Polo'       => 'vehicles/polo.jpg',
+            // Pickup Trucks
+            'Ford F-150'            => 'vehicles/pickup1.jpg',
+            'Toyota Hilux'          => 'vehicles/hilux.jpg',
+            'Nissan Navara'         => 'vehicles/navara.jpg',
+            'Chevrolet Colorado'    => 'vehicles/colorado.jpg',
+            'Mitsubishi Triton'     => 'vehicles/pickup2.jpg',
+            // Minivans
+            'Toyota Sienna'         => 'vehicles/sienna.jpg',
+            'Honda Odyssey'         => 'vehicles/odyssey.jpg',
+            'Chrysler Pacifica'     => 'vehicles/pacific.jpg',
+            // Luxury
+            'BMW 5 Series'          => 'vehicles/bmw5.jpg',
+            'Mercedes C-Class'      => 'vehicles/mercC.jpg',
+            'Audi A6'               => 'vehicles/audiA6.jpg',
+            'Lexus ES'              => 'vehicles/lexus.jpg',
+            'BMW 3 Series'          => 'vehicles/bmw3.jpg',
+            'Audi A4'               => 'vehicles/audiA4.jpg',
+            'Mercedes E-Class'      => 'vehicles/mercE.jpg',
+            // Electric
+            'Tesla Model 3'         => 'vehicles/tesla3.jpg',
+            'Tesla Model Y'         => 'vehicles/teslaY.jpg',
+            'Hyundai Ioniq 5'       => 'vehicles/ioniq5.jpg',
+            'Kia EV6'               => 'vehicles/ev6.jpg',
+            'BMW iX'                => 'vehicles/bmwix.jpg',
+            'Nissan Leaf'           => 'vehicles/leaf.jpg',
+            // Convertibles
+            'Ford Mustang GT'       => 'vehicles/mustang.jpg',
+            'Chevrolet Camaro'      => 'vehicles/camaro.jpg',
+            'BMW Z4'                => 'vehicles/bmwz4.jpg',
+            'Mazda MX-5'            => 'vehicles/convert2.jpg',
+            'Porsche Boxster'       => 'vehicles/boxster.jpg',
+            // Extra
+            'Toyota Corolla'        => 'vehicles/corolla.jpg',
+            'Honda Civic'           => 'vehicles/civic.jpg',
+            'Kia Cerato'            => 'vehicles/cerato.jpg',
+            'Mitsubishi Outlander'  => 'vehicles/outlander.jpg',
+            'Toyota Land Cruiser'   => 'vehicles/landcruiser.jpg',
+        ];
+
+        $fallbackImages = [
+            'sedan'  => 'vehicles/sedan1.jpg',
+            'suv'    => 'vehicles/suv1.jpg',
+            'hatch'  => 'vehicles/hatch1.jpg',
+            'pickup' => 'vehicles/pickup1.jpg',
+            'minivan'=> 'vehicles/minivan1.jpg',
+            'luxury' => 'vehicles/luxury1.jpg',
+            'electric'=> 'vehicles/electric1.jpg',
+            'convert'=> 'vehicles/convert1.jpg',
+            'extra'  => 'vehicles/sedan2.jpg',
+        ];
+
         $regPrefix = 'DHA-';
 
         foreach ($vehicles as $i => $v) {
@@ -104,10 +178,12 @@ class VehicleSeeder extends Seeder
 
             $vehicleId = DB::getPdo()->lastInsertId();
 
+            $img = $vehicleImages[$name] ?? ($fallbackImages[$type] ?? 'vehicles/sedan1.jpg');
+
             DB::insert("
                 INSERT INTO vehicle_images (vehicle_id, image, is_primary, sort_order, created_at, updated_at)
                 VALUES (?, ?, 1, 0, NOW(), NOW())
-            ", [$vehicleId, $imageUrls[$i % count($imageUrls)]]);
+            ", [$vehicleId, $img]);
         }
     }
 }
